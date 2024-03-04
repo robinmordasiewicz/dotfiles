e72045e9e73f39cbf198783fb0975ac9b91c47ca
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-04 13:56:58 -0500

    creating dotfiles

--- .vimrc
+++ .vimrc
@@ -1,14 +1,3 @@
-let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
-if empty(glob(data_dir . '/autoload/plug.vim'))
-  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
-endif
-
-call plug#begin('~/.vim/plugged')
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-call plug#end()
-
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
--- install.sh
+++ install.sh
@@ -4,3 +4,19 @@ set -e
 
 cp .vimrc ~/
 cp .p10k.zsh ~/
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+
+wget 'https://tpo.pe/pathogen.vim' -O ~/.vim/autoload/pathogen.vim
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
+git clone https://github.com/junegunn/fzf.vim ~/.vim/bundle/fzf.vim
+git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
+git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
+git clone https://github.com/voldikss/vim-floaterm ~/.vim/bundle/vim-floaterm
+
+mkdir -p ~/.vim/colors
+wget https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim -O ~/.vim/colors/monokai.vim
+

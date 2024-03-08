6fab258112cbcd56e200edaa5e4c7e1d232337d7
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-07 20:35:17 -0500

    updating dotfiles

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ cp .vimrc ~/
 mkdir -p ~/.vim/pack/dist/start
 git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
 
-git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
-git clone https://github.com/junegunn/fzf.vim ~/.vim/bundle/fzf.vim
-git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
-git clone https://github.com/voldikss/vim-floaterm ~/.vim/bundle/vim-floaterm
+git clone https://github.com/preservim/nerdtree ~/.vim/pack/dist/start/nerdtree
+git clone https://github.com/junegunn/fzf.vim ~/.vim/pack/dist/start/fzf
+git clone https://github.com/airblade/vim-gitgutter ~/.vim/pack/dist/start/vim-gitgutter
+git clone https://github.com/tpope/vim-fugitive ~/.vim/pack/dist/start/vim-fugitive
+git clone https://github.com/voldikss/vim-floaterm ~/.vim/pack/dist/start/vim-floaterm
 
 mkdir -p ~/.vim/colors
 wget https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim -O ~/.vim/colors/monokai.vim

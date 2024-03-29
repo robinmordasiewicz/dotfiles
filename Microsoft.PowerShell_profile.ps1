37ae64f2630a8bf65aa2b187a2f1614542fea913
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-29 18:19:41 -0400

    # remove import of Azure PowerShell module to avoid potential conflicts with other users or versions # this change is safe if you are the only user working on this machine and have no need for Azure PowerShell modules
    fix(Microsoft.PowerShell_profile.ps1): remove import of Az module to prevent potential conflicts with other users or versions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,7 +8,6 @@ if ($host.Name -eq 'ConsoleHost')
 
 Import-Module -Name Terminal-Icons
 Import-Module -Name z
-Import-Module -Name Az
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k.omp.json" | Invoke-Expression
 

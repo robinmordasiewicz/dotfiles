d9fa143bf71b2f939e6a2e018cf92bad5c518b90
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-05 10:17:29 -0400

    remove (Microsoft.PowerShell\_profile.ps1): remove commented out Import-Module lines for Terminal-Icons, z and PowerShellAI to reduce clutter and improve readability chore(Microsoft.PowerShell\_profile.ps1): clean up import statements by removing unused or unnecessary modules

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,9 +6,9 @@ if ($host.Name -eq 'ConsoleHost')
     Import-Module PSReadLine
 }
 
-# Import-Module -Name Terminal-Icons
-# Import-Module -Name z
-# Import-Module -Name PowerShellAI
+Import-Module -Name Terminal-Icons
+Import-Module -Name z
+Import-Module -Name PowerShellAI
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k.omp.json" | Invoke-Expression
 
